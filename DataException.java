a61295ed88e687993de49eaf999161414a7a17a1
Makar <zetikspb@yandex.ru>
2024-04-15 16:02:50 +0300

    Промежуточная аттестация

---
+++ DataException.java
@@ -0,0 +1,10 @@
+public class DataException extends Exception{
+    public DataException() {
+    }
+
+    public void dataException(String i) {
+        System.out.println("Exception: DataNotCorrectFormat");
+        System.out.printf("It`s not correct format: %s", i);
+        System.out.println();
+    }
+}
---
+++ SexException.java
@@ -0,0 +1,10 @@
+public class SexException extends Exception {
+    public SexException() {
+    }
+
+    public void sexException(String i) {
+        System.out.println("Exception: SexNotCorrectData");
+        System.out.printf("It`s not correct data: %s", i);
+        System.out.println();
+    }
+}
